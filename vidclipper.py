ca4aad032c830a0bb98fe12692c057045376be87
Mario Hawat <hawat.mario@gmail.com>
2022-08-02 19:11:15 +0200

    Update vidclipper.py

--- vidclipper.py
+++ vidclipper.py
@@ -1,7 +1,34 @@
 import moviepy
+import argparse
 from pathlib import Path
 from pytube import YouTube
 import moviepy.editor as me
+from moviepy.editor import *
+
+my_parser = argparse.ArgumentParser(prog='vidclipper',
+                                    description='Download and Clip a Video File')
+my_parser.add_argument('url',
+                       metavar='url',
+                       type=str,
+                       help='The URL to the video')
+                       
+my_parser.add_argument('timecodes',
+                       type=float,
+                       nargs='*',
+                       help='Input ins and outs of video')
+
+my_parser.add_argument('--speed',
+                       metavar='speed',
+                       type=float,
+                       help='Change video speed',
+                       default= 1)
+
+
+my_parser.add_argument('--conc', action='store_true', help='concatenate clipped segments')
+
+
+
+
 
 def download_video(url):
 
@@ -12,22 +39,57 @@ def download_video(url):
     downloaded_path = my_video.download('/home/kyraneth/projects/ytdownloader/videos')
     return downloaded_path
 
-def cut_video(path,i:int,o:int):
+def cut_video(path,i:int,o:int,conc,speed):
     p= Path(path)
     name = p.stem
     vid = me.VideoFileClip(path)
+    vid = vid.without_audio()
     vid = vid.subclip(i,o)
-    vid.write_videofile('/home/kyraneth/projects/ytdownloader/videos/'+name + '_' +str(i) + '_'+  str(o) + '.mp4')
+    if speed != '1':
+        vid = vid.fx(vfx.speedx, speed)
+
+    if not conc:
+        vid.write_videofile('/home/kyraneth/projects/ytdownloader/videos/'+name + '_' +str(i) + '_'+  str(o) + '.mp4')
+    return vid
+
+
 
 
 if __name__ == "__main__":
+ 
+    args = my_parser.parse_args()
+
+    url = args.url
+    
+    input = args.timecodes
+
+    conc = args.conc
+
+    speed = args.speed
 
     path = download_video(
-        "https://www.youtube.com/watch?v=PQANZXNuBAI"
+        url
     ) 
-    
 
-    cut_video(path, 95, 115)
+    vid = []
+    
+    for i in range(0, len(input)-1, 2):
+
+        vid_temp = cut_video(path, input[i], input[i+1],conc, speed)
+        vid.append(vid_temp)
+
+
+    if conc:
+
+        final = me.concatenate_videoclips(vid)
+
+        p= Path(path)
+        name = p.stem
+
+        final.write_videofile('/home/kyraneth/projects/ytdownloader/videos/'+name + '_' + 'conc.mp4')
+
+
+        
 
     p=Path(path)
     p.unlink()
